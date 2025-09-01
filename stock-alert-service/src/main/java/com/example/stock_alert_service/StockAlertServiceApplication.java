15b4ad076043d70ae006a5f0924a9eb0c7253a56
ahmad <ahmadgbolly97@gmail.com>
2025-09-01 07:34:27 +0100

    updated

--- stock-alert-service/src/main/java/com/example/stock_alert_service/StockAlertServiceApplication.java
+++ stock-alert-service/src/main/java/com/example/stock_alert_service/StockAlertServiceApplication.java
@@ -1,13 +1,22 @@
 package com.example.stock_alert_service;
 
+import com.example.stock_alert_service.stream.StockPriceStreamer;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class StockAlertServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StockAlertServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StockAlertServiceApplication.class, args);
+    }
 
+    @Bean
+    public CommandLineRunner startStreamer(StockPriceStreamer streamer) {
+        return args -> {
+            streamer.connect();
+        };
+    }
 }
